dbab0588dea7075e1787f201f6cd1f4ff4b13fe4
Vincent Lai <vlai@uvm.edu>
2023-04-14 14:50:17 -0400

    Basic database stuff

---
+++ lib/Database.php
@@ -0,0 +1,74 @@
+<?php
+class Database {
+	const DB_DEBUG = false;
+	public $pdo;
+	
+	public function __construct($databaseUser, $databaseName) {
+		// Initialize variables
+		$this->pdo = null;
+		$databasePassword = '';
+		
+		$dsn = 'mysql:host=webdb.uvm.edu;dbname=' . $databaseName;
+		
+		$userLastSeven = substr($databaseUser, (strlen($databaseUser) - 7));
+		
+		include 'password.php';
+		
+		// Check if username has _reader or _writer at the end
+		if ($userLastSeven == "_reader") {
+			$databasePassword = $reader;
+		} else if($userLastSeven == "_writer") {
+			$databasePassword = $writer;
+		}
+		
+		try {
+			$this->pdo = new PDO($dsn, $databaseUser, $databasePassword);
+			
+			if (!$this->pdo) {
+				if (self::DB_DEBUG) {
+					print PHP_EOL . '<!-- NOT Connected -->' . PHP_EOL;
+				}
+				$this->pdo = 0;
+			} else {
+				if (self::DB_DEBUG) {
+					print PHP_EOL . '<!-- Connected -->' . PHP_EOL;
+				}
+			}
+		} catch (PDOException $e) {
+			$error_message = $e->getMessage();
+			if (self::DB_DEBUG) {
+				print PHP_EOL . '<!-- Error connecting : ' . $error_message . ' -->' . PHP_EOL;
+			}
+		}
+		
+		return $this->pdo;
+	}
+	
+	public function select($query, $values = '') {
+		$statement = $this->pdo->prepare($query);
+		
+		if(is_array($values)){
+			$statement->execute($values);
+		} else {
+			$statement->execute();
+		}
+		
+		$recordSet = $statement->fetchAll(PDO::FETCH_ASSOC);
+		$statement->closeCursor();
+		return $recordSet;
+	}
+
+    public function displaySQL($sql, $data = ''){
+        $sqlText = $sql;
+        foreach ($data as $value){
+            // Look for ? and replace with the value
+            $pos = strpos($sqlText, $values);
+            if ($pos !== false) {
+                $sqlText = substr_replace($sqlText, '"' .
+        $value . '"', $pos, strlen($sqlText));
+            }
+        }
+    
+    print '<p>' . $sqlText . '</p>';
+	}
+?>
